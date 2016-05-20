1016d4284e24ac10ff0de46d923247d6aab0367c
Tim Su <tim.y.su@gmail.com>
2016-05-20 14:37:38 +1000

    Added clean task; Tidied up;

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Gulp examples for transpiling and dev pipeline
 
 ## Set up
+
 ### Install gulp and babel
 npm install -g gulp
+
+## Gulp tasks
+gulp --tasks
--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,36 @@
 let gulp = require('gulp');
 let babel = require('gulp-babel');
 let sass = require('gulp-sass');
+let del = require('del');
 
 gulp.task('default', () => {
   console.log("Gulp is working! Now type the actual command you'd like to execute.");
 });
 
+//run build before watch so it's starting with the latest
 gulp.task('watch', ['build'], () => {
   gulp.watch('src/*.js', ['build-js']);
-  // gulp.watch('src/*.css', ['build-css']);
+  gulp.watch('src/scss/**/*.scss', ['build-css']);
 });
 
-gulp.task('build-js', () => {
-	return gulp.src('src/*.js')
+gulp.task('build:js', () => {
+	return gulp.src('src/**/*.js')
 		.pipe(babel({
 			presets: ['es2015']
 		}))
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-css', () => {
-  return gulp.src('src/scss/*.scss')
+gulp.task('build:css', () => {
+  return gulp.src('src/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
 
 });
 
+gulp.task('clean', () => {
+  return del.sync('dist');
+});
 
-gulp.task('build', ['build-js']);
+
+gulp.task('build', ['build:js', 'build:css']);
